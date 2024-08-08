3a4798349d1b9478c87c7a8e550e3b45e04206cc
gorgick <gorgick2014@yandex.by>
2024-08-08 10:05:26 +0300

    view, serializer

---
+++ blogs/news/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from news.models import Blog, Like, Comment
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ('mark', 'blog', 'owner')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner_name = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ('text', 'owner_name')
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    author_name = serializers.CharField(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(read_only=True, many=True)
+    likes_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Blog
+        fields = ('id', 'title', 'owner', 'article', 'comments', 'author_name', 'likes', 'likes_count')
--- blogs/news/views.py
+++ blogs/news/views.py
@@ -1,3 +1,15 @@
+from django.db.models import Prefetch, Count, F
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from news.models import Comment, Blog
+from news.serializers import BlogSerializer
+
+
+class BlogViewSet(ModelViewSet):
+    queryset = Blog.objects.all().prefetch_related(Prefetch('comments', queryset=Comment.objects.all().annotate(
+        owner_name=F('owner__username')))).prefetch_related('likes').annotate(
+        author_name=F('owner__username')).annotate(likes_count=Count('likes'))
+    serializer_class = BlogSerializer
+
+
